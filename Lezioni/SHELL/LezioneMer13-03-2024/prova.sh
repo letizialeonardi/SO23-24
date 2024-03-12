d070df9f4eb480b52f1c844e647901acf9badf62
letizialeonardi <letizia.leonardi@unimore.it>
2024-03-12 14:53:47 +0100

    File lezione del 13 Marzo 2024

---
+++ Lezioni/SHELL/LezioneMer13-03-2024/prova.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo Valore della variabile a $a
+echo Valore della variabile PATH $PATH
+echo Valore della variabile HOME $HOME
---
+++ Lezioni/SHELL/LezioneMer13-03-2024/prova1.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo Valore della variabile a $a
+echo Valore della variabile PATH $PATH
+echo Valore della variabile HOME $HOME
+a=hello
+echo Nuovo valore della variabile a $a
