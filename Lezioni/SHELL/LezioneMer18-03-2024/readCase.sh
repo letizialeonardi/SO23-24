769bd7c2b5f2ac989999e2db4823caec391697ae
letizialeonardi <letizia.leonardi@unimore.it>
2024-03-17 12:29:39 +0100

    File lezione 18 Marzo 2024 (3 ore)

--- Lezioni/SHELL/LezioneMer18-03-2024/readCase.sh
+++ Lezioni/SHELL/LezioneMer18-03-2024/readCase.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #file comandi con primo esempio di case
 #chiediamo all'utente di fornire una risposta
-echo 'Fornire una risposta (affermativa ===> Si, si, Yes, yes)'
+echo -n 'Fornire una risposta (affermativa ===> Si, si, Yes, yes) '	#usiamo l'opzione -n per fare in modo che l'echo NON iserisca l'a-capo; N.B. abbiamo inserito un carattere spazio per maggiore leggibilita'!
 #leggiamo la risposta dell'utente
 read risposta
 #verifichiamo con un case la risposta dell'utente
